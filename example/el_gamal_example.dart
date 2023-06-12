fdcb934d256f5952b743703f83379af7aac70232
YuriiBaryshev <yura.baryshev@gmail.com>
2023-06-12 21:29:03 +0300

    create dart project

---
+++ example/el_gamal_example.dart
@@ -0,0 +1,6 @@
+import 'package:el_gamal/el_gamal.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/el_gamal.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library el_gamal;
+
+export 'src/el_gamal_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
