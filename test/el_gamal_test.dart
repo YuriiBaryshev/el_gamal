700912734e7ec47a5b540f8cb22a65690df19b52
YuriiBaryshev <yura.baryshev@gmail.com>
2023-06-14 02:42:03 +0300

    add test cases

--- test/el_gamal_test.dart
+++ test/el_gamal_test.dart
@@ -28,4 +28,29 @@ void main() {
       expect(elGamal.verifySignature(publicKey, message, signature), isTrue);
     });
   });
+
+
+  group('ElGamal encryption scheme tests', () {
+    late BigInt privateKey, publicKey;
+    final ElGamal elGamal = ElGamal();
+
+    setUp(() {
+      privateKey = elGamal.generateRand(512);
+      publicKey = elGamal.getPublicKey(privateKey);
+    });
+
+    test('encrypts and decrypts data block', () {
+      BigInt dataBlock = BigInt.from(42);
+      Map<String, BigInt> cipherText = elGamal.encryptDataBlock(publicKey, dataBlock);
+      expect(elGamal.decryptDataBlock(privateKey, cipherText), dataBlock);
+
+      dataBlock = BigInt.from(1234567890);
+      cipherText = elGamal.encryptDataBlock(publicKey, dataBlock);
+      expect(elGamal.decryptDataBlock(privateKey, cipherText), dataBlock);
+
+      dataBlock = BigInt.from(6846541231561547524);
+      cipherText = elGamal.encryptDataBlock(publicKey, dataBlock);
+      expect(elGamal.decryptDataBlock(privateKey, cipherText), dataBlock);
+    });
+  });
 }
