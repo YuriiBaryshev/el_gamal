588c704645658c310ca976de79b8f439fb38fad8
YuriiBaryshev <yura.baryshev@gmail.com>
2023-06-14 02:19:07 +0300

    add signature sheme tests

--- test/el_gamal_test.dart
+++ test/el_gamal_test.dart
@@ -1,16 +1,31 @@
+import 'dart:typed_data';
+
 import 'package:el_gamal/el_gamal.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-
+  group('ElGamal signature scheme tests', () {
+    late BigInt privateKey, publicKey;
+    final ElGamal elGamal = ElGamal();
 
     setUp(() {
-      // Additional setup goes here.
+      privateKey = elGamal.generateRand(512);
+      publicKey = elGamal.getPublicKey(privateKey);
     });
 
-    test('First Test', () {
+    test('sings and verifies signature', () {
+      Uint8List message = Uint8List.fromList([]);
+      Map<String, BigInt> signature = elGamal.sign(privateKey, message);
+      expect(elGamal.verifySignature(publicKey, message, signature), isTrue);
 
+      message = Uint8List.fromList([1, 2, 3]);
+      signature = elGamal.sign(privateKey, message);
+      expect(elGamal.verifySignature(publicKey, message, signature), isTrue);
+
+
+      message = Uint8List.fromList(List.filled(42, 42));
+      signature = elGamal.sign(privateKey, message);
+      expect(elGamal.verifySignature(publicKey, message, signature), isTrue);
     });
   });
 }
