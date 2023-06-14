ba3fa7d131cf0cde04114a72bf749222b8970db4
YuriiBaryshev <yura.baryshev@gmail.com>
2023-06-14 19:32:38 +0300

    add test cases for messages encryption and decryption

--- test/el_gamal_test.dart
+++ test/el_gamal_test.dart
@@ -62,5 +62,16 @@ void main() {
       cipherText = elGamal.encryptDataBlock(publicKey, dataBlock);
       expect(elGamal.decryptDataBlock(privateKey, cipherText), dataBlock);
     });
+
+    test('encrypts and decrypts messages', () {
+      Uint8List message = Uint8List.fromList([1, 2, 3]);
+      List<Map<String, BigInt>> cipherText = elGamal.encryptMessage(publicKey, message);
+      expect(elGamal.decryptMessage(privateKey, cipherText).sublist(0, 3), message);
+      expect(elGamal.decryptMessage(privateKey, cipherText).sublist(3), Uint8List.fromList(List.filled(508, 0)));
+
+      message = Uint8List.fromList(List.filled(1022, 42));
+      cipherText = elGamal.encryptMessage(publicKey, message);
+      expect(elGamal.decryptMessage(privateKey, cipherText), message);
+    });
   });
 }
